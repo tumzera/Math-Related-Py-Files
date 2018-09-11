968a3e16246031f7510a41da88dad18de25129c0
Tumelo <tumelomasokanye@yahoo.com>
2018-09-11 11:17:41 +0200

    Update Nbonnacci.py

--- Nbonnacci.py
+++ Nbonnacci.py
@@ -1,5 +1,8 @@
 import math as m
 def Nbonacci(n,N):
+	"""IN THE Nbonnaci fuction:
+	n -- How many numbers you want in the sequence
+	N -- The factor for generating the sequence"""
 	if n == 0:
 		print("The numbers in a N-bonnacci Sequence should be greater than 2")
 		print("Cannot find ratio with no Sequence")
@@ -24,5 +27,6 @@ def Nbonacci(n,N):
 			l.sort()
 			i = i+1
 			ratio = l[len(l)-1]/l[len(l)-2]
-		return l, ratio
+		return l
+		return ratio
 	
